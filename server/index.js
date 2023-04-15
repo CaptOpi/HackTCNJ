305028a16c08f4cefdfe48efba1e844ad18769f4
CaptOpi <ahmed10698.ak@gmail.com>
2023-04-15 12:57:10 -0400

    Backend changes

--- server/index.js
+++ server/index.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
-
+const UserModel = require('./models/Users')
 mongoose.connect(
     "mongodb+srv://mongodb:YKbOaRaybaqzi5HM@cluster0.xjinasp.mongodb.net/?retryWrites=true&w=majority"
 );
+
+app.get("/getUsers", (req, res) => {
+    UserModel.find({}, (err, result) => {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json(result);
+        }
+    });
+});
+
+app.post()
 app.listen(8080, () => {
     console.log("SERVER RUNS")
 });
--- server/models/Users.js
+++ server/models/Users.js
@@ -1,12 +1,30 @@
 const mongoose = require('mongoose')
 
-const UserSchema = new mongoose.SchemaType({
+const UserSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        minlength: 1,
+        maxlength: 40
     },
     email: {
         type: String,
-        required: true
+        required: true,
+        unique: true,
+        validate: {
+            validator: function(v) {
+                return /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(v);
+            },
+            message: props =>`${props.value} is not a valid email.`  
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+        maxlength: 20
     }
 });
+
+const UserModel = mongoose.model("users", UserSchema);
+module.exports = UserModel
