59cc7ee15e71583790b8192fef7ba3b4b34ed318
Kusum Gandham <koolkusum10@gmail.com>
2023-04-15 16:08:56 -0400

    help

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,32 +1,44 @@
-// import React, { useEffect, useState} from 'react'
-// import axios from "axios"
-// import { useNavigate, Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { createUser } from './api'; // assuming the api.js file is in the same directory as this file
 
-// function Login(){
-//     const[email, setEmail] = useState("")
-//     const[password, setPassword] = useState("")
-//     async function submit(e){
-//         e.preventDefault()
-//         try{
-//             await axios.post("http:")
-//         }
-//         catch{
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-//         }
-//     }
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const user = await createUser({ email, password });
+      console.log(user); // do something with the user data, like redirect to a new page
+    } catch (error) {
+      console.error(error); // handle errors
+    }
+  };
 
-//     return(
-//         <div className="login">
-//         <hi>Login</hi>
-//             <form action ="POST">
-//                 <input type="email" onchange = {(e)=>{setEmail(e.target.value)}} name="Email" id=""/>
-//                 <input type="password" onchange = {(e)=>{setPassword(e.target.value)}} name="Password" id=""/>
-//                 <input type="submit" onClick={submit}/>
-//             </form>
-//             <br />
-//             <p> OR</p>
-//             <br />
-//             <Link to="/signup"></Link>
-//         </div>
-//     )
-// }
+  return (
+    <div className="login">
+      <h1>Login</h1>
+      <form onSubmit={handleLogin}>
+        <label>
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
