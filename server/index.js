4cfb617fb38bfaae768e0a88c2f7328130ebf8d2
CaptOpi <ahmed10698.ak@gmail.com>
2023-04-15 12:35:16 -0400

    App Server running on localhost

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+const mongoose = require('mongoose');
 
 mongoose.connect(
-    "mongodb+srv://mongodb:XIfYlirMbyCLtZOi@cluster0.xjinasp.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://mongodb:YKbOaRaybaqzi5HM@cluster0.xjinasp.mongodb.net/?retryWrites=true&w=majority"
 );
 app.listen(8080, () => {
     console.log("SERVER RUNS")
