4792c06658fab3dc3c0ef99d769184936b3407ff
CaptOpi <ahmed10698.ak@gmail.com>
2023-04-15 13:06:08 -0400

    hello world

--- server/index.js
+++ server/index.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
 const UserModel = require('./models/Users')
+
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+
 mongoose.connect(
     "mongodb+srv://mongodb:YKbOaRaybaqzi5HM@cluster0.xjinasp.mongodb.net/?retryWrites=true&w=majority"
 );
@@ -16,7 +23,12 @@ app.get("/getUsers", (req, res) => {
     });
 });
 
-app.post()
-app.listen(8080, () => {
+app.post("/createUser", async (req, res) => {
+    const user = req.body;
+    const newUser = new UserModel(user);
+    await newUser.save()
+    res.json(user)
+})
+app.listen(5000, () => {
     console.log("SERVER RUNS")
 });
